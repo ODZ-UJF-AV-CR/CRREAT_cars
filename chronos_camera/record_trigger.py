658d7a6b6bdc3e7ae794d5de8a23e5dcbc9f2352
Jakub Kákona <kaklik@mlab.cz>
2021-07-09 10:50:44 +0200

    Update record_trigger.py

--- chronos_camera/record_trigger.py
+++ chronos_camera/record_trigger.py
@@ -2,5 +2,7 @@
 
 import requests
 
+#inspired by https://github.com/krontech/chronos-examples/tree/master/python3
+
 post = requests.post('http://chronos.lan/control/startFilesave', json = {'format': 'h264', 'device': 'mmcblk1p1'})
 print(post.reason)
