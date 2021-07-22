c788491f82ac6d37cf184a360d55f9da57b53372
Jakub Kakona <kaklik@mlab.cz>
2021-07-22 19:52:08 +0000

    The first working version.

--- position_tracking/position_measurement.py
+++ position_tracking/position_measurement.py
@@ -23,40 +23,45 @@ def stream_ubx(**kwargs):
     Stream output
     """
 
-    try:
-        rover1 = kwargs.get("port", rover1_port).strip('"')
-        baud = int(kwargs.get("baud", BAUD))
-        timeout = int(kwargs.get("timeout", TIMEOUT))
-        ubxonly = int(kwargs.get("ubxonly", 0))
-        rawformat = int(kwargs.get("raw", 0))
-        print(
-            f"\nStreaming from {port} at {baud} baud in",
-            f"{'raw' if rawformat else 'parsed'} format...\n",
-        )
-        stream = Serial(rover1, baud, timeout=timeout)
-        ubr = UBXReader(stream, ubxonly=ubxonly)
-        for (raw, parsed) in ubr:
-            if rawformat:
-                print(raw)
-            else:
-                print(parsed)
+    rover1 = kwargs.get("rover1_port", rover1_port).strip('"')
+    baud = int(kwargs.get("baud", BAUD))
+    timeout = int(kwargs.get("timeout", TIMEOUT))
+    ubxonly = int(kwargs.get("ubxonly", 0))
+    rawformat = int(kwargs.get("raw", 0))
+    print(
+        f"\nStreaming from {rover1_port} at {baud} baud in",
+        f"{'raw' if rawformat else 'parsed'} format...\n",
+    )
 
-        rover2 = kwargs.get("port", rover2_port).strip('"')
-        baud = int(kwargs.get("baud", BAUD))
-        timeout = int(kwargs.get("timeout", TIMEOUT))
-        ubxonly = int(kwargs.get("ubxonly", 0))
-        rawformat = int(kwargs.get("raw", 0))
-        print(
-            f"\nStreaming from {port} at {baud} baud in",
-            f"{'raw' if rawformat else 'parsed'} format...\n",
-        )
-        stream = Serial(rover2, baud, timeout=timeout)
-        ubr = UBXReader(stream, ubxonly=ubxonly)
-        for (raw, parsed) in ubr:
-            if rawformat:
-                print(raw)
-            else:
-                print(parsed)
+    rover2 = kwargs.get("rover2_port", rover2_port).strip('"')
+    baud = int(kwargs.get("baud", BAUD))
+    timeout = int(kwargs.get("timeout", TIMEOUT))
+    ubxonly = int(kwargs.get("ubxonly", 0))
+    rawformat = int(kwargs.get("raw", 0))
+    print(
+        f"\nStreaming from {rover2_port} at {baud} baud in",
+        f"{'raw' if rawformat else 'parsed'} format...\n",
+    )
+
+    stream1 = Serial(rover1, baud, timeout=timeout)
+    stream2 = Serial(rover2, baud, timeout=timeout)
+
+    try:
+        while(True):
+
+            ubr = UBXReader(stream1, ubxonly=ubxonly)
+            for (raw, parsed) in ubr:
+                if parsed.identity == 'NAV-RELPOSNED':
+                    print("Rover 1")
+                    print(parsed)
+                    break
+
+            ubr = UBXReader(stream2, ubxonly=ubxonly)
+            for (raw, parsed) in ubr:
+                if parsed.identity == 'NAV-RELPOSNED':
+                    print("Rover 2")
+                    print(parsed)
+                    break
 
     except KeyboardInterrupt:
         print("\nStreaming terminated by user\n")
