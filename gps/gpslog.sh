63ffeff0977feeb15219546072f7a7ac44020e6f
Roman Dvořák <romandvorak@mlab.cz>
2022-07-15 00:52:43 +0200

    Update gpslog.sh

--- gps/gpslog.sh
+++ gps/gpslog.sh
@@ -5,6 +5,6 @@ echo " "
 mkdir /data/gps/ -p >/dev/null 2>&1
 echo "Soupstim zaznam"
 
-gpspipe -R -P -w -o /data/gps_$(date +"%Y%m%d_%H%M%S").log
+gpspipe -R -P -w -o /data/gps/gps_$(date +"%Y%m%d_%H%M%S").log
 
 echo "Konec"
