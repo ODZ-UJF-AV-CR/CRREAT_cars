ab2b46bb26b4bc713726238df865286e381b27e5
Jakub Kakona <kaklik@mlab.cz>
2022-08-05 20:19:13 +0000

    Attempt to use parallel command.

--- gps/gpslog.sh
+++ gps/gpslog.sh
@@ -5,12 +5,10 @@ echo " "
 STATION=${STATION:-'CARx'}
 
 mkdir /data/gps/ -p >/dev/null 2>&1
-echo "Soupstim zaznam"
+echo "Spoustim zaznam"
 
 gpsd /dev/ttyACM0 /dev/ttyACM1 /dev/ttyACM2 -F /run/gpsd.sock
 
-gpspipe -R -P -w -o /data/gps/base_gps_${STATION}_$(date +"%Y%m%d_%H%M%S").log ::/dev/ttyACM0 &
-gpspipe -R -P -w -o /data/gps/rover1_gps_${STATION}_$(date +"%Y%m%d_%H%M%S").log ::/dev/ttyACM1 &
-gpspipe -R -P -w -o /data/gps/rover2_gps_${STATION}_$(date +"%Y%m%d_%H%M%S").log ::/dev/ttyACM2 &
+parallel ::: gpspipe -R -P -w -o /data/gps/base_gps_${STATION}_$(date +"%Y%m%d_%H%M%S").log ::/dev/ttyACM0 gpspipe -R -P -w -o /data/gps/rover1_gps_${STATION}_$(date +"%Y%m%d_%H%M%S").log ::/dev/ttyACM1 gpspipe -R -P -w -o /data/gps/rover2_gps_${STATION}_$(date +"%Y%m%d_%H%M%S").log ::/dev/ttyACM2
 
 echo "Konec"
