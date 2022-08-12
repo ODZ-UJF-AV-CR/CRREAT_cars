af4d7d184717ddd29a73bda6d15fb9e8fb98bbfc
Jakub Kakona <kaklik@mlab.cz>
2022-08-12 21:46:02 +0000

    removing parallel command.

--- gps/gpslog.sh
+++ gps/gpslog.sh
@@ -7,8 +7,10 @@ STATION=${STATION:-'CARx'}
 mkdir /data/gps/ -p >/dev/null 2>&1
 echo "Spoustim zaznam"
 
-gpsd /dev/ttyACM0 /dev/ttyACM1 /dev/ttyACM2 -F /run/gpsd.sock
+gpsd /dev/ttyACM0 /dev/ttyACM1 /dev/ttyACM2 -F /run/gpsd.sock &
 
-parallel ::: gpspipe -R -P -w -o /data/gps/base_gps_${STATION}_$(date +"%Y%m%d_%H%M%S").log ::/dev/ttyACM0 gpspipe -R -P -w -o /data/gps/rover1_gps_${STATION}_$(date +"%Y%m%d_%H%M%S").log ::/dev/ttyACM1 gpspipe -R -P -w -o /data/gps/rover2_gps_${STATION}_$(date +"%Y%m%d_%H%M%S").log ::/dev/ttyACM2
+gpspipe -R -P -w -o /data/gps/base_gps_${STATION}_$(date +"%Y%m%d_%H%M%S").log ::/dev/ttyACM0 &
+gpspipe -R -P -w -o /data/gps/rover1_gps_${STATION}_$(date +"%Y%m%d_%H%M%S").log ::/dev/ttyACM1 &
+gpspipe -R -P -w -o /data/gps/rover2_gps_${STATION}_$(date +"%Y%m%d_%H%M%S").log ::/dev/ttyACM2 &
 
 echo "Konec"
