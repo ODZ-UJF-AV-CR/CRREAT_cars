a11de8b9298a42538d3b50b6d63467759d5a4377
root <romandvorak@mlab.cz>
2022-09-10 12:17:18 +0000

    Fix confusing name with base gps for RTK.

--- gps/gpslog_simple.sh
+++ gps/gpslog_simple.sh
@@ -8,6 +8,6 @@ mkdir /data/gps/ -p >/dev/null 2>&1
 echo "Spoustim zaznam"
 
 sleep 10
-gpspipe -R -P -w -o /data/gps/base_gps_${STATION}_$(date +"%Y%m%d_%H%M%S").log 192.168.1.1 
+gpspipe -R -P -w -o /data/gps/omnia_gps_${STATION}_$(date +"%Y%m%d_%H%M%S").log 192.168.1.1 
 
 echo "Konec"
